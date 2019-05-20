70d73a97d8a162c8b6ad736669e2aeaedd5f8013
Victor Afanasev <vicfreefly@gmail.com>
2019-05-20 19:59:19 +0300

    Fix Bigcommerce::V3::ProductVariants.update method

--- lib/bigcommerce/resources/v3/products_variants.rb
+++ lib/bigcommerce/resources/v3/products_variants.rb
@@ -8,6 +8,13 @@ module Bigcommerce
       include Bigcommerce::SubresourceActions.new uri: 'catalog/products/%d/variants?id:in=%d'
 
       property :data
+
+      def self.update(parent_id, resource_id, params = {})
+        raise ArgumentError if [parent_id, resource_id].any?(&:nil?)
+
+        builded_path = "catalog/products/#{parent_id}/variants/#{resource_id}"
+        put builded_path, params
+      end
     end
   end
 end
